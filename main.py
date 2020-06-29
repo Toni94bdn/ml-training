62758f5cd8c7c3005d7ad66748185b103ea309ef
Toni <toni.forcada.sans@gmail.com>
2020-06-30 00:58:04 +0200

    Pg 57

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+np.random.seed(42)
+single_data_point = np.random.randint(0, 100, 2)
+print(single_data_point)
